71ed4b2578e054b6e08d89c8e5120f0bdfd51a45
Jorge Mestre Tomás <73013114+jmestret@users.noreply.github.com>
2024-08-08 11:45:36 +0200

    Update rtree.cpp

--- src/rtree.cpp
+++ src/rtree.cpp
@@ -3,7 +3,7 @@
 // Enable C++11 via this plugin to suppress 'long long' errors
 // [[Rcpp::plugins("cpp11")]]
 
-// Some of this code based on https://gallery.rcpp.org/articles/Rtree-examples/
+// Some of this code based on http://gallery.rcpp.org/articles/Rtree-examples/
 
 #include <vector>
 #include <Rcpp.h>
@@ -11,7 +11,6 @@
 #include <boost/geometry/geometries/point.hpp>
 #include <boost/geometry/geometries/box.hpp>
 #include <boost/geometry/index/rtree.hpp>
-
 using namespace Rcpp;
 
 // Mnemonics
@@ -23,8 +22,10 @@ typedef std::pair<point_t, unsigned int> value_t;
 
 class RTreeCpp {
 public:
-  // Constructor, creates R-Tree on points in mat and accepts Earth radius R
-  RTreeCpp(NumericMatrix mat, double R) : earth_radius(R) {
+
+  // Constructor, creates R-Tree on points in mat
+  // mat must have 2 columns with x and y coordinates of points
+  RTreeCpp(NumericMatrix mat) {
     coords = clone(mat);
     int size = mat.nrow();
     NumericVector x = mat(_, 0);
@@ -38,25 +39,30 @@ public:
     }
 
     // Create rtree using packing algorithm
-    bgi::rtree<value_t, bgi::quadratic<16>> rtree_new(point_value_pairs);
+    bgi::rtree<value_t, bgi::quadratic<16> > rtree_new(point_value_pairs);
     rtree_ = rtree_new;
   }
 
   // Within-box lookup
+  // box_vec is [llx, lly, urx, ury] coordinates
   std::vector<int> intersects(NumericVector box_vec) {
+    // Find points within box_vec
     box query_box(point_t(box_vec[0], box_vec[1]), point_t(box_vec[2], box_vec[3]));
     std::vector<value_t> result_n;
     rtree_.query(bgi::intersects(query_box), std::back_inserter(result_n));
 
+    // Get just the indices of the points
     std::vector<int> indexes;
-    for (const auto& value : result_n) {
+    std::vector<value_t>::iterator itr;
+    for (itr = result_n.begin(); itr != result_n.end(); ++itr) {
+      value_t value = *itr;
       indexes.push_back(value.second);
     }
     return indexes;
   }
 
   // Get Haversine distances between point and points referenced by their indexes
-  std::vector<double> get_haversine_distances(NumericVector point_vec, std::vector<int> indexes) {
+  std::vector<double> get_haversine_distances(NumericVector point_vec, std::vector<int> indexes, double earth_radius) {
     double lat1 = point_vec[1] * M_PI / 180.0; // Latitude
     double lon1 = point_vec[0] * M_PI / 180.0; // Longitude
 
@@ -96,66 +102,79 @@ public:
   }
 
   // Get indices of points within distance of point
-  std::vector<int> within_distance(NumericVector point_vec, double distance, std::string distance_metric) {
+  // Note: Returns R indices (starting at 1, not 0!)
+  std::vector<int> within_distance(NumericVector point_vec, double distance, std::string distance_metric, double earth_radius) {
+    // Candidate points are within a square with side 2 * distance
     std::vector<int> indist_indexes;
     NumericVector box_vec = NumericVector::create(
       point_vec[0] - distance, point_vec[1] - distance,
       point_vec[0] + distance, point_vec[1] + distance);
 
     std::vector<int> inbox_indexes = intersects(box_vec);
-    if (inbox_indexes.empty()) {
+    if (inbox_indexes.size() == 0) {
       return indist_indexes;
     }
 
+    // Filter candidates by the actual distance
     std::vector<double> inbox_distances;
     if (distance_metric == "haversine") {
-      inbox_distances = get_haversine_distances(point_vec, inbox_indexes);
+      inbox_distances = get_haversine_distances(point_vec, inbox_indexes, earth_radius);
     } else if (distance_metric == "euclidean") {
       inbox_distances = get_euclidean_distances(point_vec, inbox_indexes);
+    } else {
+      Rcpp::stop("Invalid distance metric. Use 'haversine' or 'euclidean'.");
     }
 
     for (unsigned int i = 0; i < inbox_distances.size(); i++) {
       if (inbox_distances[i] <= distance) {
-        indist_indexes.push_back(inbox_indexes[i] + 1); // R indices start at 1
+        indist_indexes.push_back(inbox_indexes[i] + 1);
       }
     }
     return indist_indexes;
   }
 
   // Multi point version of within_distance
-  List within_distance_list(NumericMatrix point_mat, double distance, std::string distance_metric) {
+  // Note: Returns R indices (starting at 1, not 0!)
+  List within_distance_list(NumericMatrix point_mat, double distance, std::string distance_metric, double earth_radius) {
     List indist_indexes_ls(point_mat.nrow());
     for (int i = 0; i < point_mat.nrow(); i++) {
       NumericVector point_vec = point_mat(i, _);
-      std::vector<int> indist_indexes = within_distance(point_vec, distance, distance_metric);
+      std::vector<int> indist_indexes = within_distance(point_vec, distance, distance_metric, earth_radius);
       indist_indexes_ls(i) = wrap(indist_indexes);
     }
     return indist_indexes_ls;
   }
 
   // Counting version of within_distance_list
-  std::vector<int> count_within_distance_list(NumericMatrix point_mat, double distance, std::string distance_metric) {
+  // For each point in point_mat, returns the number of points within distance.
+  // This uses much less memory than within_distance_list when point_mat is
+  // large and there are many points within distance.
+  std::vector<int> count_within_distance_list(NumericMatrix point_mat, double distance, std::string distance_metric, double earth_radius) {
     std::vector<int> indist_indexes_ls(point_mat.nrow());
     for (int i = 0; i < point_mat.nrow(); i++) {
       NumericVector point_vec = point_mat(i, _);
-      std::vector<int> indist_indexes = within_distance(point_vec, distance, distance_metric);
+      std::vector<int> indist_indexes = within_distance(point_vec, distance, distance_metric, earth_radius);
       indist_indexes_ls[i] = indist_indexes.size();
     }
     return indist_indexes_ls;
   }
 
   // KNN
+  // Note: Returns R indices (starting at 1, not 0!)
   std::vector<int> knn(NumericVector point, unsigned int n) {
     std::vector<value_t> result_n;
     rtree_.query(bgi::nearest(point_t(point[0], point[1]), n), std::back_inserter(result_n));
     std::vector<int> indexes;
-    for (const auto& value : result_n) {
+    std::vector<value_t>::iterator itr;
+    for (itr = result_n.begin(); itr != result_n.end(); ++itr) {
+      value_t value = *itr;
       indexes.push_back(value.second + 1);
     }
     return indexes;
   }
 
   // Multi point version of KNN
+  // Note: Returns R indices (starting at 1, not 0!)
   List knn_list(NumericMatrix point_mat, unsigned int n) {
     List knn_indexes_ls(point_mat.nrow());
     for (int i = 0; i < point_mat.nrow(); i++) {
@@ -169,7 +188,6 @@ public:
 private:
   bgi::rtree<value_t, bgi::quadratic<16>> rtree_;
   NumericMatrix coords;
-  double earth_radius; // Store the Earth radius
 };
 
 RCPP_MODULE(rtreecpp) {
